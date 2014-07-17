d3a61a3ab9fea73564a1f0c28a793a384245f81c
mwmaleks <mwmaleks@gmx.com>
2014-07-17 20:34:56 +0400

    fixed the same as here https://github.com/Orlac/groupinputs/commit/00137654ba8d27ebf7f34387d288a92c404f4405

--- jquery.groupinputs.js
+++ jquery.groupinputs.js
@@ -5,25 +5,27 @@
  * MIT Licensed
  */
 (function($) {
-'use strict';
+    'use strict';
 
-var isOpera = !!window.opera,
-    MAXLENGTH = 'maxlength';
+    var isOpera = !!window.opera,
+        MAXLENGTH = 'maxlength';
 
-function caret(node, start, end) {
-    var range;
-    if (start !== undefined) {
-        if (node.setSelectionRange) {
-            node.setSelectionRange(start, end);
-        // IE, "else" for opera 10
-        } else if (document.selection && document.selection.createRange) {
-            range = node.createTextRange();
-            range.collapse(true);
-            range.moveEnd('character', end);
-            range.moveStart('character', start);
-            range.select();
+    function caret(node, start, end) {
+        var range
+          , dup;
+        if (start !== undefined) {
+            if (node.setSelectionRange) {
+                node.setSelectionRange(start, end);
+                // IE, "else" for opera 10
+            } else if (document.selection && document.selection.createRange) {
+                range = node.createTextRange();
+                range.collapse(true);
+                range.moveEnd('character', end);
+                range.moveStart('character', start);
+                range.select();
+            }
+            return;
         }
-    } else {
         start = 0;
         end = 0;
         if ('selectionStart' in node) {
@@ -31,7 +33,7 @@ function caret(node, start, end) {
             end = node.selectionEnd;
         } else if (node.createTextRange) {
             range = document.selection.createRange();
-            var dup = range.duplicate();
+            dup = range.duplicate();
 
             if (range.parentElement() === node) {
                 start = -dup.moveStart('character', -100000);
@@ -43,112 +45,112 @@ function caret(node, start, end) {
             end: end
         };
     }
-}
 
-$.fn.groupinputs = function() {
-    // iOS does not support input.focus()
-    if (/iPad|iPhone/.test(navigator.platform)) {
-        return this;
-    }
-
-    var inputs = this,
-        inputsMaxlength = [],
-        totalMaxlength = 0;
-
-    function afterPaste(elem, options) {
-        // for example, inputs value before paste: [00|2 ] [33  ], need paste: 1111
-        // now state: [001111|2] [33  ]
-
-        var firstValue = elem[0].value,
-            caretEnd = caret(elem[0]).end, // in webkit start: 2, end: 6
-            left = firstValue.slice(0, caretEnd), // 001111
-            right = firstValue.slice(caretEnd), // 2
-            rightFreeSpace = options.maxlength - right.length, // 3
-            isSetFocus = false,
-            buffer, newCaretStart, i;
-
-        for (i = inputs.length - 1; i > options.index; i--) {
-            rightFreeSpace += inputsMaxlength[i] - inputs[i].value.length;
+    $.fn.groupinputs = function() {
+        // iOS does not support input.focus()
+        if (/iPad|iPhone/.test(navigator.platform)) {
+            return this;
         }
 
-        if (left.length > rightFreeSpace) {
-            left = left.slice(0, rightFreeSpace); // 001111.slice(0, 5)
-            newCaretStart = rightFreeSpace;
-        } else {
-            newCaretStart = caretEnd;
-        }
+        var inputs = this,
+            inputsMaxlength = [],
+            totalMaxlength = 0;
 
-        if (firstValue.length > options.maxlength) {
-            elem[0].value = (left + right).slice(0, options.maxlength); // [0011] [33  ]
+        function afterPaste(elem, options) {
+            // for example, inputs value before paste: [00|2 ] [33  ], need paste: 1111
+            // now state: [001111|2] [33  ]
 
-            // caret remains on input
-            if (newCaretStart <= options.maxlength) {
-                caret(elem[0], newCaretStart, newCaretStart);
-                isSetFocus = true;
+            //var firstValue = elem[0].value,
+            var firstValue = filterString(elem[0].value),
+                caretEnd = caret(elem[0]).end, // in webkit start: 2, end: 6
+                left = firstValue.slice(0, caretEnd), // 001111
+                right = firstValue.slice(caretEnd), // 2
+                rightFreeSpace = options.maxlength - right.length, // 3
+                isSetFocus = false,
+                buffer, newCaretStart, i;
+
+            for (i = inputs.length - 1; i > options.index; i--) {
+                rightFreeSpace += inputsMaxlength[i] - inputs[i].value.length;
             }
 
-            buffer = (left + right).slice(options.maxlength); // 112
+            if (left.length > rightFreeSpace) {
+                left = left.slice(0, rightFreeSpace); // 001111.slice(0, 5)
+                newCaretStart = rightFreeSpace;
+            } else {
+                newCaretStart = caretEnd;
+            }
 
-            if (buffer.length) {
-                newCaretStart -= Math.min(options.maxlength, left.length);
-                var maxlength, valLength;
-                while (inputs[++i]) {
-                    maxlength = inputsMaxlength[i];
-                    buffer += inputs[i].value; // 11233
+            if (firstValue.length > options.maxlength) {
+                elem[0].value = (left + right).slice(0, options.maxlength); // [0011] [33  ]
 
-                    inputs.eq(i)
-                        .val(buffer.slice(0, maxlength))
-                        .change();
+                // caret remains on input
+                if (newCaretStart <= options.maxlength) {
+                    caret(elem[0], newCaretStart, newCaretStart);
+                    isSetFocus = true;
+                }
 
-                    if (buffer.length <= maxlength) {
-                        break;
-                    }
+                buffer = (left + right).slice(options.maxlength); // 112
 
-                    valLength = inputs[i].value.length;
+                if (buffer.length) {
+                    newCaretStart -= Math.min(options.maxlength, left.length);
+                    var maxlength, valLength;
+                    while (inputs[++i]) {
+                        maxlength = inputsMaxlength[i];
+                        buffer += inputs[i].value; // 11233
 
-                    if (!isSetFocus) {
-                        if (newCaretStart < maxlength) {
-                            isSetFocus = true;
-                            inputs.eq(i).focus();
-                            caret(inputs[i], newCaretStart, newCaretStart);
+                        inputs.eq(i)
+                            .val(buffer.slice(0, maxlength))
+                            .change();
+
+                        if (buffer.length <= maxlength) {
+                            break;
                         }
-                        newCaretStart -= valLength;
+
+                        valLength = inputs[i].value.length;
+
+                        if (!isSetFocus) {
+                            if (newCaretStart < maxlength) {
+                                isSetFocus = true;
+                                inputs.eq(i).focus();
+                                caret(inputs[i], newCaretStart, newCaretStart);
+                            }
+                            newCaretStart -= valLength;
+                        }
+                        buffer = buffer.slice(maxlength);
                     }
-                    buffer = buffer.slice(maxlength);
+                }
+                if (!isSetFocus) {
+                    // setTimeout may be necessary for chrome and safari (https://bugs.webkit.org/show_bug.cgi?id=56271)
+                    inputs.eq(i).focus();
+                    caret(inputs[i], newCaretStart, newCaretStart);
                 }
             }
-            if (!isSetFocus) {
-                // setTimeout may be necessary for chrome and safari (https://bugs.webkit.org/show_bug.cgi?id=56271)
-                inputs.eq(i).focus();
-                caret(inputs[i], newCaretStart, newCaretStart);
-            }
-        }
-    }
-
-    function handler(e) {
-        var eventType = e.type,
-            options = e.data,
-            elem = e.data.elem,
-            index = e.data.index,
-            caretPos;
-
-        if (isOpera) { // last check 12
-            if (eventType === 'keypress') {
-                eventType = 'keydown';
-            }
         }
 
-        var LEFT_CODE = 37,
-            BACKSPACE_CODE = 8,
-            DELETE_CODE = 46,
-            RIGHT_CODE = 39;
+        function handler(e) {
+            var eventType = e.type,
+                options = e.data,
+                elem = e.data.elem,
+                index = e.data.index,
+                caretPos;
+
+            if (isOpera) { // last check 12
+                if (eventType === 'keypress') {
+                    eventType = 'keydown';
+                }
+            }
+
+            var LEFT_CODE = 37,
+                BACKSPACE_CODE = 8,
+                DELETE_CODE = 46,
+                RIGHT_CODE = 39;
 
             if (eventType === 'keydown' && e.keyCode === RIGHT_CODE) {
                 caretPos = caret(elem[0]);
                 if (
                     caretPos.start === this.value.length && // caret is last
                     index !== inputs.length - 1 // input is no last
-                ) {
+                    ) {
                     inputs.eq(index + 1).focus();
                     caret(inputs[index + 1], 0, 0);
                     e.preventDefault(); // no next motion
@@ -160,7 +162,7 @@ $.fn.groupinputs = function() {
                     caretPos.start === caretPos.end &&
                     caretPos.start === 0 && // caret is first
                     index !== 0 // input is no first
-                ) {
+                    ) {
                     var toFocus = inputs.eq(index - 1),
                         lengthToFocus = toFocus.val().length;
                     toFocus.focus();
@@ -172,7 +174,7 @@ $.fn.groupinputs = function() {
             }
             if (eventType === 'keyup' ||
                 eventType === 'keydown') { // repeat in FF10, Webkit, IE
-            //case 'keypress': // repeat in FF10, Opera 11
+                //case 'keypress': // repeat in FF10, Opera 11
                 // ignore system key. ex. shift
                 if (e.keyCode < 48) {
                     return;
@@ -195,7 +197,7 @@ $.fn.groupinputs = function() {
                     caretPos.start === this.value.length && // caret is last
                     index !== inputs.length - 1 && // input is no last
                     this.value.length === options.maxlength
-                ) {
+                    ) {
                     inputs.eq(index + 1).focus();
                     caret(inputs[index + 1], 0, 0);
                 }
@@ -203,9 +205,9 @@ $.fn.groupinputs = function() {
             if (eventType === 'paste') {
                 elem.attr(MAXLENGTH, totalMaxlength);
             }
-/*            case 'keypress':
-                elem.attr(MAXLENGTH, options.maxlength + 1);
-                break;*/
+            /*            case 'keypress':
+             elem.attr(MAXLENGTH, options.maxlength + 1);
+             break;*/
             if (eventType === 'propertychange' || // IE8
                 eventType === 'input') { // webkit set cursor position as [00|11112]
                 // after paste
@@ -220,28 +222,37 @@ $.fn.groupinputs = function() {
             if (eventType === 'input' && isOpera) {
                 afterPaste(elem, options);
             }
-    }
+        }
 
-    inputs.each(function(i) {
-        var elem = inputs.eq(i),
-            maxlength = +elem.attr(MAXLENGTH);
+        function filterString(str){
+            str=str.replace(/\s+/g, '');
+            str=str.replace(/\-/g, '');
+            return str;
+        }
 
-        totalMaxlength += maxlength;
-        inputsMaxlength.push(maxlength);
+        inputs.each(function(i) {
+            var elem = inputs.eq(i),
+                maxlength = +elem.attr(MAXLENGTH);
 
-        elem.on('keydown keypress keyup input paste propertychange', {
-            elem: elem,
-            index: i,
-            maxlength: maxlength
-        }, handler);
-    });
+            totalMaxlength += maxlength;
+            inputsMaxlength.push(maxlength);
 
-    // opera not support paste event
-    if (isOpera) {
-        inputs.attr(MAXLENGTH, totalMaxlength);
-    }
+            elem.bind('keydown keypress keyup input paste propertychange', function(e){
+                e.data={
+                    elem: elem,
+                    index: i,
+                    maxlength: maxlength
+                };
+                var ctx=$(elem).get(0);
+                handler.apply(ctx, [e]);
+            } );
+        });
 
-    return inputs;
-};
+        // opera not support paste event
+        if (isOpera) {
+            inputs.attr(MAXLENGTH, totalMaxlength);
+        }
+        return inputs;
+    };
 
 }(jQuery));
